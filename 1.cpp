34fa865275c2f6456690085f1cde6f42cfd18656
Askiatic <108981467+Askiatic@users.noreply.github.com>
2026-01-06 20:14:37 +0800

    完成了修改

---
+++ 1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#define int long long
+
+inline void run() {
+    
+}
+
+signed main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int T = 1;
+    std::cin >> T;
+    while (T--) run();
+
+    return 0;
+}
